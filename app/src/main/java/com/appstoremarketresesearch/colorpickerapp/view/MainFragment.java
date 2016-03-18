3fbe226ef4af5039cf1077dcb616fd970c19fb9c
danielgsalas <danielgsalas@yahoo.com>
2016-03-18 07:17:15 -0700

    moved Fragment into view package

--- app/src/main/java/com/appstoremarketresesearch/colorpickerapp/view/MainFragment.java
+++ app/src/main/java/com/appstoremarketresesearch/colorpickerapp/view/MainFragment.java
@@ -1,4 +1,4 @@
-package com.appstoremarketresesearch.android_simplecolorpicker;
+package com.appstoremarketresesearch.colorpickerapp.view;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -6,12 +6,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.appstoremarketresesearch.android_simplecolorpicker.R;
+
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainFragment extends Fragment {
 
-    public MainActivityFragment() {
+    public MainFragment() {
     }
 
     @Override
